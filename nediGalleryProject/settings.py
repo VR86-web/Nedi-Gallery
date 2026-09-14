330bdb765bf6e62d79237f1647157923a357c0a2
VR86-web <145848782+VR86-web@users.noreply.github.com>
2026-09-14 23:46:29 +0200

    register the art description context processor in settings.py

--- nediGalleryProject/settings.py
+++ nediGalleryProject/settings.py
@@ -79,6 +79,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'nediGalleryProject.common.context_processors.instagram_pics',
+                'nediGalleryProject.common.context_processors.art_description'
             ],
         },
     },
